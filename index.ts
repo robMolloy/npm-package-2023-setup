cc69e03e0c130adceca6d55d64424c2654ee9384
Rob Molloy <robmolloy@hotmail.co.uk>
2023-01-18 06:16:38 +0000

    sub2

--- index.ts
+++ index.ts
@@ -1,2 +1,2 @@
 export const add = (numb1: number, num2: number) => numb1 + num2;
-export const sub = (numb1: number, num2: number) => numb1 - num2;
+export const sub2 = (numb1: number, num2: number) => numb1 - num2;
